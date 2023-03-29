f429a6e30374292a6e4505bf1c6dd11f0231cf81
NomedXD <coovshow1@gmail.com>
2023-03-29 17:45:16 +0300

    save

---
+++ MultiplayerProject/MultiplayerProject_Server.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <vector>
+
+#include "NetworkServer.h"
+
+using namespace sf;
+using namespace std;
+
+NetworkServer netS;
+
+
+int main()
+{
+	netS.init();
+	
+
+	Packet packet;
+	packet << "DATA";
+	for (int i = 0; i < netS.clientsVec.size(); i++)
+	{
+		packet << netS.clientsVec[i].name << netS.clientsVec[i].pos.x << netS.clientsVec[i].pos.y << netS.clientsVec[i].turned;
+	}
+
+	while (true)
+	{
+		netS.registerNewClients();
+		netS.sendConnectedClientsRecords();
+
+		if (netS.sendDataToAll(packet) == Socket::Status::Done)
+		{
+			packet.clear();
+			packet << "DATA";
+			for (int i = 0; i < netS.clientsVec.size(); i++)
+			{
+				packet << netS.clientsVec[i].name << netS.clientsVec[i].pos.x << netS.clientsVec[i].pos.y << netS.clientsVec[i].turned;
+			}
+		}
+
+
+		unsigned int receivedClientIndex;
+		if (netS.receiveData(receivedClientIndex) == Socket::Status::Done)
+		{
+			if (netS.clientsVec[receivedClientIndex].rDataPacket.getDataSize() > 0)
+			{
+				string s;
+				if (netS.clientsVec[receivedClientIndex].rDataPacket >> s)
+				{
+					if (s == "DATA")
+					{
+						float x, y;
+						if (netS.clientsVec[receivedClientIndex].rDataPacket >> x)
+						{
+							netS.clientsVec[receivedClientIndex].pos.x = x;
+						}
+						if (netS.clientsVec[receivedClientIndex].rDataPacket >> y)
+						{
+							netS.clientsVec[receivedClientIndex].pos.y = y;
+						}
+						bool turn;
+						if (netS.clientsVec[receivedClientIndex].rDataPacket >> turn)
+						{
+							netS.clientsVec[receivedClientIndex].turned = turn;
+						}
+						netS.clientsVec[receivedClientIndex].rDataPacket.clear();
+					}
+				}
+			}
+		}
+	}
+
+
+	getchar();
+	return 0;
+}
